2c8bdf6afb66b06f384eb624552bf55dd0d0e2fd
Lawrence O. Caulker <locaulker@gmail.com>
2022-08-17 12:10:25 -0400

    end L161

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react"
-import Accordion from "./components/Accordion"
+import Search from "./components/Search"
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a Front-End JavaScript Framework.",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS Library among engineers.",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
-]
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a Front-End JavaScript Framework.",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS Library among engineers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ]
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   )
 }

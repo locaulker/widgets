73aba37de370535a2a8ee3d866a6eda9ef718ddb
Lawrence O. Caulker <locaulker@gmail.com>
2022-08-21 03:45:02 -0400

    end Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react"
-import Search from "./components/Search"
+import React, { useState } from "react"
+import Dropdown from "./components/Dropdown"
 
 // const items = [
 //   {
@@ -16,10 +16,37 @@ import Search from "./components/Search"
 //   },
 // ]
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+]
+
 export default () => {
+  const [selected, setSelected] = useState(options[0])
+  const [showDropdown, setShowDropdown] = useState(true)
+
   return (
     <div>
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   )
 }
